064d7c98830eceb9d70ec8a7aab6c16fd698db01
oakieland <48298728+oakieland@users.noreply.github.com>
2023-11-19 14:07:05 -0600

    Added Type Hinting

--- television.py
+++ television.py
@@ -1,27 +1,27 @@
 class Television:
-    MIN_VOLUME = 0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
 
     def __init__(self):
         '''
         Method to Initialize TV Object
         '''
-        self.__status = False
-        self.__muted = False
-        self.__volume = Television.MIN_VOLUME
-        self.__channel = Television.MIN_CHANNEL
-
+        self.__status: bool = False
+        self.__muted: bool = False
+        self.__volume: int = Television.MIN_VOLUME
+        self.__channel: int = Television.MIN_CHANNEL
+        
 
     def power(self):
         '''
         Method to mute television
         '''
         if self.__status == False:
-            self.__status = True
+            self.__status: bool = True
         else:
-            self.__status = False
+            self.__status: bool = False
 
     def mute(self):
         '''
@@ -29,9 +29,9 @@ class Television:
         '''
         if self.__status:
             if self.__muted == True:
-                self.__muted = False
+                self.__muted: bool = False
             else:
-                self.__muted = False
+                self.__muted: bool = True
 
     def channel_up(self):
         '''
@@ -58,7 +58,7 @@ class Television:
         Method to turn down tv volume.
         '''
         if self.__status:
-            self.__muted = False
+            self.__muted: bool = False
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
@@ -67,7 +67,7 @@ class Television:
         Method to turn up tv volume.
         '''
         if self.__status:
-            self.__muted = False
+            self.__muted: bool = False
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
     def __str__(self) -> str:
